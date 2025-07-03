291c92ba580e9e284348616939b38227c2a4e0ee
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-07-03 17:04:52 +0100

    fix: cors error

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL:
     typeof config.apiBaseUrl === 'string' ? config.apiBaseUrl : undefined,
+  withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
