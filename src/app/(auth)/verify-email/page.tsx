3f74ee2fbf8be5d727a17f410fdfbbe900e64090
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-07-04 13:54:56 +0100

    fix: verify page bug

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,51 +1,23 @@
-'use client';
 
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 import ResendVerificationEmail from '@/components/auth/ResendEmailVerification';
 import AlertMessage from '@/components/common/AlertMessage';
 
-export default function Page() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get('email');
+interface PageProps {
+  searchParams: Promise<{ email?: string }>;
+}
 
-  const [alert, setAlert] = useState<{
-    type: 'success' | 'error';
-    message: string;
-    id?: number;
-  } | null>(null);
-
-  const showAlert = (alertObj: {
-    type: 'success' | 'error';
-    message: string;
-  }) => {
-    setAlert({ ...alertObj, id: Date.now() });
-  };
-
-  useEffect(() => {
-    if (!email) {
-      showAlert({
-        type: 'error',
-        message: 'Email is required to resend verification link.',
-      });
-    }
-  }, [email]);
+export default async function Page({ searchParams }: PageProps) {
+  const { email } = await searchParams;
 
   if (!email) {
     return (
       <div className="max-w-md mx-auto mt-10">
-        {alert && (
-          <AlertMessage
-            key={alert.id}
-            type={alert.type}
-            message={alert.message}
-            duration={7000}
-            className="mb-4"
-          />
-        )}
+        <AlertMessage
+          type="error"
+          message='Email is required to resend verification link.'
+        />
       </div>
     );
   }
-
   return <ResendVerificationEmail email={email} />;
 }
