c7751ed5016db2222dbcc423e943e5b16e63819b
Akinfenwa Taiwo Christopher <101477417+AChris-T@users.noreply.github.com>
2025-07-07 13:48:08 +0100

    feat:added custom ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,71 @@
 'use client';
-import React from 'react';
+
+import { DownloadIcon } from '@/assets/icons';
+import Button from '@/components/ui/button/Button';
+import CardSkeleton from '@/components/ui/skeleton/CardSkeleton';
+import React, { useEffect, useState } from 'react';
 
 export default function Ecommerce() {
-  return <div className="grid grid-cols-12 gap-4 md:gap-6">Welcome</div>;
+  const [loading, setLoading] = useState<'initial' | 'button' | 'none'>(
+    'initial'
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading('none');
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClick = async () => {
+    setLoading('button');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setLoading('none');
+    alert('Action completed!');
+  };
+
+  const isInitialLoading = loading === 'initial';
+  const isButtonLoading = loading === 'button';
+
+  return (
+    <div className="space-y-6">
+      {isInitialLoading ? (
+        <>
+          <CardSkeleton />
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {Array.from({ length: 6 }).map((_, i) => (
+              <CardSkeleton key={i} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="space-y-4">
+          <Button>Hello</Button>
+
+          <Button className="px-10 py-4 text-xs" variant="primary">
+            Submit
+          </Button>
+
+          <Button disabled>Disable</Button>
+
+          <Button
+            variant="outline"
+            rightIcon={<DownloadIcon width={14} height={14} />}
+          >
+            Download Note
+          </Button>
+
+          <Button
+            onClick={handleClick}
+            isLoading={isButtonLoading}
+            fullWidth
+            variant="primary"
+            disabled={isButtonLoading}
+          >
+            Click Me
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
