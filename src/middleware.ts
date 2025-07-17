45d98d5268249b64199d85e8721c05667377ac2e
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-07-17 20:17:57 +0100

    fix: redirect bug 1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getSessionCookie } from './lib/session';
 
 export async function middleware(request: NextRequest) {
-  const token = await getSessionCookie();
   const { pathname } = request.nextUrl;
   const authPages = ['/signin', '/signup'];
   const protectedRoutes = ['/dashboard'];
+  const token = request.cookies.get('SESSION_COOKIE')?.value;
 
   if (token && authPages.includes(pathname)) {
-    const url = request.nextUrl.clone();
-    const redirect = url.searchParams.get('redirect') || '/dashboard';
-    return NextResponse.redirect(new URL(redirect, request.url));
+    const redirectPath =
+      request.nextUrl.searchParams.get('redirect') || '/dashboard';
+    const redirectUrl = new URL(redirectPath, request.url);
+    return NextResponse.redirect(redirectUrl);
   }
 
   if (!token && protectedRoutes.some((route) => pathname.startsWith(route))) {
