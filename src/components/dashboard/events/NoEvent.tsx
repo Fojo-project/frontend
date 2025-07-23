da8827d6b4cb12eac3103a8065de4b9d5a86fcfe
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-07-23 15:17:44 +0100

    feat: avatar

--- src/components/dashboard/events/NoEvent.tsx
+++ src/components/dashboard/events/NoEvent.tsx
@@ -1,11 +1,10 @@
 
 import Image from 'next/image';
-import NoEventImage from "../../../../public/images/event/No-Event.png"
 
 export default function NoEvent() {
   return (
     <div className="text-center text-gray-500 py-10 flex flex-col items-center">
-      <Image src={NoEventImage} alt="No events" width={150} height={150} />
+      <Image src="/images/event/No-Event.png" alt="No events" width={150} height={150} />
       <p className="mt-4 text-lg font-normal mb-1 font-open-sans">No events Events Yet</p>
       <p className="text-sm font-open-sans">Watch Out for Live Events</p>
     </div>
